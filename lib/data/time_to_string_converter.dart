5148f727eb30b8c032b4696ada3392e374f1e1dd
Adel Shagaliev <a.shagaliev@innopolis.university>
2024-05-23 13:59:43 +0300

    feat[app]: covered time converter with unit tests

--- lib/data/time_to_string_converter.dart
+++ lib/data/time_to_string_converter.dart
@@ -12,7 +12,13 @@ class TimeToStringConverter {
 
   String _convertAsCommentTime(DateTime time) {
     final Duration timeToConvert = DateTime.now().difference(time);
-    if (timeToConvert.inHours > 0) {
+    if (timeToConvert.inDays > 365) {
+      return '${timeToConvert.inDays ~/ 365}г';
+    } else if (timeToConvert.inDays > 30) {
+      return '${timeToConvert.inDays ~/ 30}мес';
+    } else if (timeToConvert.inHours > 24) {
+      return '${timeToConvert.inHours ~/ 24}д';
+    } else if (timeToConvert.inHours > 0) {
       return '${timeToConvert.inHours}ч';
     } else if (timeToConvert.inMinutes > 0) {
       return '${timeToConvert.inMinutes}мин';
---
+++ test/time_to_string_converter_test.dart
@@ -0,0 +1,91 @@
+import 'package:coursor_tiktok/data/time_to_string_converter.dart';
+import 'package:coursor_tiktok/domain/enums/time_convert_type.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Convert time for comments', () {
+    test('Seconds ago', () {
+      // arrange
+      final converter = TimeToStringConverter();
+
+      // act
+      final String result = converter.convert(
+        DateTime.now().subtract(const Duration(seconds: 5)),
+        TimeConvertType.comment,
+      );
+
+      // assert
+      expect(result, equals('5с'));
+    });
+
+    test('Minutes ago', () {
+      // arrange
+      final converter = TimeToStringConverter();
+
+      // act
+      final String result = converter.convert(
+        DateTime.now().subtract(const Duration(minutes: 3)),
+        TimeConvertType.comment,
+      );
+
+      // assert
+      expect(result, equals('3мин'));
+    });
+
+    test('Hours ago', () {
+      // arrange
+      final converter = TimeToStringConverter();
+
+      // act
+      final String result = converter.convert(
+        DateTime.now().subtract(const Duration(hours: 3)),
+        TimeConvertType.comment,
+      );
+
+      // assert
+      expect(result, equals('3ч'));
+    });
+
+    test('Days ago', () {
+      // arrange
+      final converter = TimeToStringConverter();
+
+      // act
+      final String result = converter.convert(
+        DateTime.now().subtract(const Duration(days: 3)),
+        TimeConvertType.comment,
+      );
+
+      // assert
+      expect(result, equals('3д'));
+    });
+
+    test('Months ago', () {
+      // arrange
+      final converter = TimeToStringConverter();
+
+      // act
+      final String result = converter.convert(
+        DateTime.now().subtract(const Duration(days: 76)),
+        TimeConvertType.comment,
+      );
+
+      // assert
+      expect(result, equals('2мес'));
+    });
+
+    test('Years ago', () {
+      // arrange
+      final converter = TimeToStringConverter();
+
+      // act
+      final String result = converter.convert(
+        DateTime.now().subtract(const Duration(days: 500)),
+        TimeConvertType.comment,
+      );
+
+      // assert
+      expect(result, equals('1г'));
+    });
+  });
+}
